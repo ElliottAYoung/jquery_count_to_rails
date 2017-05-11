d7f3c4d3d1e9a9a05ab7802d8add803f899b9a14
ElliottAYoung <elliott.a.young@gmail.com>
2017-05-11 16:17:49 -0500

    Add primary script

--- lib/jquery_count_to_rails.rb
+++ lib/jquery_count_to_rails.rb
@@ -1,3 +1,14 @@
 module JqueryCountToRails
-  # Your code goes here...
+  if defined? ::Rails
+    if ::Rails.version.to_s < '3.1'
+      require 'jquery-countdown-rails/railtie'
+    else
+      require 'jquery-countdown-rails/engine'
+    end
+  elsif defined? ::Sprockets
+    require 'jquery-countdown-rails/sprockets'
+  else
+    require 'jquery-countdown-rails/errors/unsupported_platform_error'
+    raise UnsupportedPlatformError
+  end
 end
